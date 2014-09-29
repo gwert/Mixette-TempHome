5420e9a0cc61dd40768b77f34ae190957adc5f90
Graham Wert <grahamwert@gmail.com>
2014-09-29 14:48:14 -0400

    Basic static layout imported.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MixtagramTemp::Application.config.secret_key_base = 'c161b3a0d46910da3f85456ded2b2b86e7591b3d98daa49d39af3d0f30da285eea96e5c86628dcd07576b0c66e8326b870bed904c27164e308092a0243aba2a3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MixtagramTemp::Application.config.session_store :cookie_store, key: '_Mixtagram-Temp_session'
